ec8e83fa2300eac26771fb0beb7d9c5688e8a824
Francisco Macedo <macedo270701@gmail.com>
2024-12-01 13:40:13 -0300

    feat<main class>: added main

---
+++ src/main/java/com/nativenews/api/SpringNativeNewsApplication.java
@@ -0,0 +1,11 @@
+package com.nativenews.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeNewsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeNewsApplication.class, args);
+    }
+}
