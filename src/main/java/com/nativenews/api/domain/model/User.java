8cfc83fa4b5816f0173776ce32e21d8facabac2c
Francisco Macedo <macedo270701@gmail.com>
2024-12-01 13:36:28 -0300

    feat<user dto>: added user DTO (completo)

---
+++ src/main/java/com/nativenews/api/domain/model/User.java
@@ -0,0 +1,42 @@
+package com.nativenews.api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // construtor sem data
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
